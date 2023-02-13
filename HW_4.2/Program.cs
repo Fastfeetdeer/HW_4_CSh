2ab65e38b7032e13be2f11661194fd3a1cb1ab90
e.demin <demsq@mail.ru>
2023-02-13 15:23:33 +0300

    Complete task 4.2

--- HW_4.2/Program.cs
+++ HW_4.2/Program.cs
@@ -1,2 +1,19 @@
 ﻿// ДЗ 4.2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
+Console.WriteLine("Инициализация программы");
+int SumOfDig(int a)
+{
+    int sumdig = 0;
+    while ( a > 0 )
+    {
+        sumdig += a % 10;
+        a = a / 10;
+    }
+    return sumdig;
+}
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Сумма чисел в цифре {a} = {SumOfDig(a)}");
+Console.WriteLine("Программа завершила работу");
